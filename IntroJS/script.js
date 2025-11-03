7f473a85ba937f54209cad2fc795be97bf88e232
Sunil <suniljob2005@gmail.com>
2025-11-03 10:02:56 +0530

    some projects added

--- IntroJS/script.js
+++ IntroJS/script.js
@@ -1,7 +1,33 @@
-console.log("Hi welcome to the chimakurthy")
+console.log("Hi welcome to the Ongole")
 console.warn("this is warning")
 console.error("Error")
 
+// alert('this is alert') // it shows an alert message
+// var c= prompt('enter the number')  // it is taking input from user
+
+// var val=confirm('how is that') // it is shown true or false
 
 var a=10;
-console.log(a)
+var b=25;
+console.log(a+b)
+
+const s= 20 // we can not override the value
+var h= 24  // we can override the value
+// var h= prompt('enter a num')
+console.log(s)
+
+// s=25
+// console.log(s)
+
+var i='welcome'
+var u='Ongole'
+// console.log(i+ to +u)
+
+var n= null
+console.log(n)
+
+var si= 'hi welcome'
+console.log(si)
+
+var v;
+console.log(v)
---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+let username={
+    name:'sunil',
+    age:23,
+    city:"ongole"
+}
+
+console.log(username.age)
+
+var v= '4'
+
+var s ='3'
+
+var e=Number(s)
+
+console.log(typeof e)
